81fa716d5becea8c72de53f5f064ac288013fe66
Vaibhav <vaibhavagarwal693@gmail.com>
2024-03-09 12:57:58 +0530

    course route

--- api/route/course.js
+++ api/route/course.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { addCourse, enrollCourse,getCourseData } from "../controller/course.js";
+import { addCourse, enrollCourse,getCourseData,getAllCourse } from "../controller/course.js";
 import { isAuthenticated } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/add",isAuthenticated, addCourse);//tutor will add
 router.post("/enroll",isAuthenticated, enrollCourse);// user will enroll
+router.get("/getAll",getAllCourse)
 router.get("/:id", getCourseData);// user will enroll
+
+
 
 // router.post("/update", isAuthenticated,editProfile)
 
