db4b231806526a1b43c76f359b9bb95b7d727937
Aditya <adityachauhan2501@gmail.com>
2024-03-09 19:17:11 +0530

    profile-updated

--- client/src/student/pages/CourseDetails.js
+++ client/src/student/pages/CourseDetails.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import SelectDuration from "../components/SelectDuration";
@@ -6,23 +6,16 @@ import { Button } from "@mui/material";
 import Review from "../components/Review";
 import TimeSlots from "../components/TimeSlots";
 import Notes from "../components/Notes";
-import axios from "axios"
+import axios from "axios";
 
-import {Context} from "../../index.js"
-
+import { Context } from "../../index.js";
 
 export default function CourseDetails() {
-  const [coursename, setCourseName] = useState("");
-  const [tutuorName, setTutuorName] = useState("");
-  const [studentEnrolled, setStudentEnrolled] = useState("");
-  const [ratings, setRatings] = useState("");
-  const [price, setPrice] = useState();
+  const [coursedetails, setCourseDetails] = useState({});
   const [isEnrolled, setIsEnrolled] = useState(false);
-
-  const {user,tutor}=useContext(Context)
+  const { user, tutor } = useContext(Context);
 
   const { id } = useParams();
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,12 +27,13 @@ export default function CourseDetails() {
           }
         );
 
-        console.log(tutor)
-      
-    // const userId="65ec319acc1045217409f901"
+        //console.log(tutor);
+
+        // const userId="65ec319acc1045217409f901"
 
         const data1 = response.data.result;
         console.log(data1);
+        setCourseDetails(data1);
 
         // const tutorDataPromises = data1.map(async (element) => {
         //   const tutor_id = element.tutor_id;
@@ -66,11 +60,10 @@ export default function CourseDetails() {
     fetchData();
   }, []);
 
-
   return (
     <>
       <Navbar />
-      <h1 className="mt-5 mx-10 text-3xl font-bold">Course-Name</h1>
+      <h1 className="mt-5 mx-10 text-3xl font-bold">{coursedetails.name}</h1>
 
       {isEnrolled === true ? (
         <div className="flex gap-4">
@@ -87,10 +80,14 @@ export default function CourseDetails() {
             <div className="mx-10 mt-10 h-[20rem] w-[30rem] bg-gray-200"></div>
             <div className="mx-10 mt-10">
               <p className="my-3 py-2">TutorName</p>
-              <p className="my-3 py-2"> StudentsEnrolled</p>
-              <p className="my-3 py-2">Ratings</p>
-              <p className="my-3 py-2">Starting On</p>
-              <p className="my-3 py-2">Price</p>
+              <p className="my-3 py-2">
+                Enrolled-students : {coursedetails.enrolled_students}
+              </p>
+              <p className="my-3 py-2">Ratings : {coursedetails.rating}</p>
+              <p className="my-3 py-2">
+                Starting on : {coursedetails.createdAt}
+              </p>
+              <p className="my-3 py-2">Price : {coursedetails.pricing}</p>
             </div>
           </div>
 
