73ecfa44d057c2ee1d349b0f5eb8920f210d8c6c
Aditya <adityachauhan2501@gmail.com>
2024-03-09 13:05:15 +0530

    notes-added

--- client/src/student/pages/CourseDetails.js
+++ client/src/student/pages/CourseDetails.js
@@ -4,6 +4,8 @@ import Navbar from "../components/Navbar";
 import SelectDuration from "../components/SelectDuration";
 import { Button } from "@mui/material";
 import Review from "../components/Review";
+import TimeSlots from "../components/TimeSlots";
+import Notes from "../components/Notes";
 
 /**
  *
@@ -17,6 +19,7 @@ export default function CourseDetails() {
   const [studentEnrolled, setStudentEnrolled] = useState("");
   const [ratings, setRatings] = useState("");
   const [price, setPrice] = useState();
+  const [isEnrolled, setIsEnrolled] = useState(true);
 
   const { id } = useParams();
 
@@ -25,27 +28,41 @@ export default function CourseDetails() {
       <Navbar />
       <h1 className="mt-5 mx-10 text-3xl font-bold">Course-Name</h1>
 
-      <div className="flex gap-4">
-        <div className="mx-10 mt-10 h-[20rem] w-[30rem] bg-gray-200"></div>
-        <div className="mx-10 mt-10">
-          <p className="my-3 py-2">TutorName</p>
-          <p className="my-3 py-2"> StudentsEnrolled</p>
-          <p className="my-3 py-2">Ratings</p>
-          <p className="my-3 py-2">Starting On</p>
-          <p className="my-3 py-2">Price</p>
+      {isEnrolled === true ? (
+        <div className="flex gap-4">
+          <div className="mx-10 mt-10 h-[20rem] w-[30rem] bg-gray-200">
+            live-class-video
+          </div>
+          <div className="mx-10 mt-10">
+            <Notes />
+          </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="flex gap-4">
+            <div className="mx-10 mt-10 h-[20rem] w-[30rem] bg-gray-200"></div>
+            <div className="mx-10 mt-10">
+              <p className="my-3 py-2">TutorName</p>
+              <p className="my-3 py-2"> StudentsEnrolled</p>
+              <p className="my-3 py-2">Ratings</p>
+              <p className="my-3 py-2">Starting On</p>
+              <p className="my-3 py-2">Price</p>
+            </div>
+          </div>
 
-      <div className="mt-10 mx-10 flex flex-row justify-between">
-        <SelectDuration />
-        <Button variant="contained" color="success">
-          Purchase
-        </Button>
-      </div>
-      <div className="mt-10 mx-10">
-        <h1 className="text-3xl mt-20 mb-10 mx-12">Reviews</h1>
-        <Review />
-      </div>
+          <div className="mt-10 mx-10 flex flex-row justify-between">
+            <SelectDuration />
+            <TimeSlots />
+            <Button variant="contained" color="success">
+              Purchase
+            </Button>
+          </div>
+          <div className="mt-10 mx-10">
+            <h1 className="text-3xl mt-20 mb-10 mx-12">Reviews</h1>
+            <Review />
+          </div>
+        </>
+      )}
     </>
   );
 }
